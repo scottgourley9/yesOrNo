fe66960805093e69be3591103a0daf6d4f9e06ea
Scott Gourley <scottgourley9@gmail.com>
2017-01-01 12:15:03 -0700

    e

--- public/main.js
+++ public/main.js
@@ -38,7 +38,7 @@ console.log(params);
     }
 });
   })
-  $('button').on('click', function(){
+  $('.no').on('click', function(){
     var complaint = $('textarea').val()
     $.post('https://www.in-sightreviews.com/api/complaint/' + messageId + '/' + complaint + '/' + customerId + '/' + userId, {complaint: complaint}, function(result){
       $('.noFormSection').hide()
